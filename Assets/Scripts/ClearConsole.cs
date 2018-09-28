0140dd57ae726e62c1773f6eec53563c3965c890
Theoogamar <haydenwswift@gmail.com>
2018-09-28 23:22:16 +1000

    Revert "Revert "intial commit""
    This reverts commit a3c80e33d283714c060618eda745b924a47221c4.

---
+++ Assets/Scripts/ClearConsole.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class ClearConsole : MonoBehaviour {
+
+    //public void ClearLog()
+    //{
+    //    var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+    //    var type = assembly.GetType("UnityEditor.LogEntries");
+    //    var method = type.GetMethod("Clear");
+    //    method.Invoke(new object(), null);
+    //}
+}
