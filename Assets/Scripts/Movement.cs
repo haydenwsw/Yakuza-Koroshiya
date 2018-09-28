faaec85c79a651e3c6a06e54040c2c178af1779d
Theoogamar <haydenwswift@gmail.com>
2018-09-28 23:20:12 +1000

    intial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour {
+
+    [SerializeField]
+    private float Speed = 5f;
+
+    [SerializeField]
+    private float Sensitivity = 10f;
+
+    private Controller con;
+
+    private bool zoom;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // get controller script
+        con = GetComponent<Controller>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get movement velocity
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical"); 
+
+        Vector3 vecH = transform.right * x;
+        Vector3 vecV = transform.forward * z;
+
+        Vector3 vel = (vecH + vecV).normalized * Speed;
+
+        // apply movement
+        con.Move(vel);
+
+        // camera y axis
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 vecY = new Vector3(0f, yRot, 0f) * Sensitivity;
+
+        con.moveMouseY(vecY);
+
+        // camera x axis
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 vecX = new Vector3(xRot, 0f, 0f) * Sensitivity;
+
+        con.moveMouseX(vecX);
+
+        // jump
+        float jump = Input.GetAxisRaw("Jump");
+
+        Vector3 vecUP = new Vector3(0f, jump, 0f) * Sensitivity;
+
+        con.Jump(vecUP);
+
+        // change FOV
+        if (Input.GetKeyDown("c"))
+        {
+            zoom = true;
+            con.cameraZoom(zoom);
+        }
+        if (Input.GetKeyUp("c"))
+        {
+            zoom = false;
+            con.cameraZoom(zoom);
+        }
+    }
+}
