7ad200859ee62ae808484c402872986eb2e585ab
Theoogamar <haydenwswift@gmail.com>
2018-10-24 18:59:30 +1100

    add base AI
    24/10/2018

---
+++ Assets/SCRIPTS/AI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour {
+
+    public float LookRadius = 10f;
+
+    public GameObject Player;
+
+    Transform target;
+    NavMeshAgent agent;
+
+	void Start ()
+    {
+        target = Player.transform;
+        agent = GetComponent<NavMeshAgent>();
+
+        transform.Rotate(-90, 0, 0);
+	}
+	
+	void Update ()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= LookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                // attack the player
+            }
+        }
+	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, LookRadius);
+    }
+}
