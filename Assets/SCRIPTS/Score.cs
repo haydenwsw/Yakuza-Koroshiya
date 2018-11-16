3dd1da1de2207cfc5573e96e9a01df9470a2dcd9
Hayden <haydenwswift@gmail.com>
2018-11-16 16:45:00 +1100

    More on death and scoring
    16/11/2018

--- Assets/SCRIPTS/Score.cs
+++ Assets/SCRIPTS/Score.cs
@@ -11,16 +11,26 @@ public class Score : MonoBehaviour {
 
     public GameObject plusTen;
 
+    public GameObject plusOne;
+
     private float time;
 
     private GameObject obj = null;
 
     public void AddScore(int i, Vector3 pos, Quaternion rot)
     {
-        ScoreCount += i;
-        ScoreText.text = ScoreCount.ToString();
-
-        obj = Instantiate(plusTen, pos + (Vector3.up), rot) as GameObject;
+        if (i == 1)
+        {
+            ScoreCount += 1;
+            ScoreText.text = ScoreCount.ToString();
+            obj = Instantiate(plusOne, pos + (Vector3.up * 1.5f), rot) as GameObject;
+        }
+        if (i == 0)
+        {
+            ScoreCount += 10;
+            ScoreText.text = ScoreCount.ToString();
+            obj = Instantiate(plusTen, pos + (Vector3.up), rot) as GameObject;
+        }
     }
 
     private void Update()
@@ -34,7 +44,7 @@ public class Score : MonoBehaviour {
         int children = transform.GetChild(1).childCount;
         for (int i = 0; i < children; ++i)
         {
-            if (transform.GetChild(1).GetChild(i).tag == "AI")
+            if (transform.GetChild(1).GetChild(i).tag == "AI" || transform.GetChild(1).GetChild(i).tag == "RifleAmmo" || transform.GetChild(1).GetChild(i).tag == "ShotgunAmmo")
             {
                 Destroy(transform.GetChild(1).GetChild(i).gameObject);
             }
---
+++ Assets/SCRIPTS/ScoreEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreEffect : MonoBehaviour {
+
+    private Vector3 face;
+
+    private float time = 0;
+
+    void Update ()
+    {
+        time += Time.deltaTime;
+        if (time >= 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        face = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Vector3 dir = (face - transform.position).normalized;
+        Quaternion lookRot = Quaternion.LookRotation(new Vector3(dir.x, 0f, dir.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * 20);
+    }
+}
